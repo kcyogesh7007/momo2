812df4c65467a51841e6d2134820da36f244de14
Yogesh khadka <kcyogesh7007@gmail.com>
2026-01-27 17:20:47 +0545

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    userEmail: {
+      type: String,
+      required: [true, "Email must be provided"],
+    },
+    userPassword: {
+      type: String,
+      required: [true, "password must be provided"],
+    },
+    userName: {
+      type: String,
+      required: [true, "userName must be provided"],
+    },
+    userPhoneNumber: {
+      type: Number,
+      required: [true, "Phone Number must be provided"],
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
