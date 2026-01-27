4b74a5a202ab46360b0ef835b9f66e28d7669acc
Yogesh khadka <kcyogesh7007@gmail.com>
2026-01-27 23:06:27 +0545

    add auth

--- controller/auth/authController.js
+++ controller/auth/authController.js
@@ -1,6 +1,8 @@
 const User = require("../../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const otpGenerator = require("otp-generator");
+const sendEmail = require("../../services/sendEmail");
 
 exports.registerUser = async (req, res) => {
   const { email, userName, password, phoneNumber } = req.body;
@@ -52,5 +54,92 @@ exports.loginUser = async (req, res) => {
   res.status(200).json({
     message: "User loggedIn successfully",
     token,
+  });
+};
+
+exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({
+      message: "Please provide email",
+    });
+  }
+  const userExist = await User.findOne({ userEmail: email });
+  if (!userExist) {
+    return res.status(400).json({
+      message: "User with that email doesnot exist",
+    });
+  }
+  const otp = otpGenerator.generate(6, {
+    upperCaseAlphabets: false,
+    lowerCaseAlphabets: false,
+    specialChars: false,
+  });
+  await sendEmail({
+    email,
+    subject: "Your OTP for DigitalMomo",
+    message: `Your One time password for DigtalMomo is ${otp}. Don't share with anyone.`,
+  });
+  userExist.otp = otp;
+  await userExist.save();
+  res.status(200).json({
+    message: "Otp email sent successfully",
+  });
+};
+
+exports.verifyOtp = async (req, res) => {
+  const { email, otp } = req.body;
+  if (!email || !otp) {
+    return res.status(400).json({
+      message: "Please provide otp and email",
+    });
+  }
+  const userExist = await User.findOne({ userEmail: email });
+  if (!userExist) {
+    return res.status(400).json({
+      message: "User with that email doesnot exist",
+    });
+  }
+  if (userExist.otp !== otp) {
+    return res.status(400).json({
+      message: "Incorrect otp",
+    });
+  }
+  userExist.isOtpVerified = true;
+  userExist.otp = undefined;
+  await userExist.save();
+  res.status(200).json({
+    message: "Otp verified successfully",
+  });
+};
+
+exports.resetPassword = async (req, res) => {
+  const { email, newPassword, confirmPassword } = req.body;
+  if (!email || !newPassword || !confirmPassword) {
+    return res.status(400).json({
+      message: "Please provide email,new Password and confirm Password",
+    });
+  }
+  const userExist = await User.findOne({ userEmail: email });
+  if (!userExist) {
+    return res.status(400).json({
+      message: "User with that email doesnot exist",
+    });
+  }
+  if (newPassword !== confirmPassword) {
+    return res.status(400).json({
+      message: "New password and confirm password doesnot match",
+    });
+  }
+  if (!userExist.isOtpVerified) {
+    return res.status(400).json({
+      message: "You cannot perform this action",
+    });
+  }
+  userExist.userPassword = bcrypt.hashSync(newPassword, 10);
+  userExist.isOtpVerified = false;
+  await userExist.save();
+  res.status(200).json({
+    message: "Password reset successfully",
   });
 };
